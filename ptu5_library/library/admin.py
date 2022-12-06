b1b8f209162cab4588b2a0e5973043164b2b3da6
Anuzaya96 <filipciuk10@gmail.com>
2022-12-06 17:53:08 +0200

    .

---
+++ ptu5_library/library/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from . import models
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = models.BookInstance
+    extra = 0
+    # readonly_fields = ('unique_id', )
+    can_delete = False
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = (BookInstanceInline, )
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('unique_id', 'book', 'status', 'is_overdue', 'due_back', 'reader')
+    list_filter = ('status', 'due_back')
+    readonly_fields = ('unique_id', 'is_overdue')
+    search_fields = ('unique_id', 'book__title', 'book__author__last_name__exact', 'reader__last_name')
+    list_editable = ('status', 'due_back', 'reader')
+
+    fieldsets = (
+        ('General', {'fields': ('unique_id', 'book')}),
+        ('Availability', {'fields': (('status', 'due_back', 'is_overdue'), 'reader')}),
+    )
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'display_books')
+    list_display_links = ('last_name', )
+
+
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display = ('book', 'reader', 'created_at')
+
+admin.site.register(models.Author, AuthorAdmin)
+admin.site.register(models.Genre)
+admin.site.register(models.Book, BookAdmin)
+admin.site.register(models.BookInstance, BookInstanceAdmin)
+admin.site.register(models.BookReview, BookReviewAdmin)
+
+
---
+++ ptu5_library/library/views.py
@@ -0,0 +1,169 @@
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.db.models import Q 
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic.edit import FormMixin
+from django.urls import reverse, reverse_lazy
+from . models import Genre, Author, Book, BookInstance
+from . forms import BookReviewForm, BookInstanceForm, BookInstanceUpdateForm
+
+
+def index(request):
+    #return HttpResponse("Sveiki atvyke!")
+
+    book_count = Book.objects.count()
+    book_instance_count = BookInstance.objects.count()
+    #panaudojam filtra, available
+    book_instances_available_count = BookInstance.objects.filter(status='a').count()
+    author_count = Author.objects.count()
+    visits_count = request.session.get('visits_count', 1)
+    request.session['visits_count'] = visits_count + 1
+
+    context = {
+        'book_count': book_count,
+        'book_instance_count': book_instance_count,
+        'book_instances_available_count': book_instances_available_count,
+        'author_count': author_count,
+        'genre_count': Genre.objects.count(),
+        'visits_count': visits_count,
+    }
+
+    return render(request, 'library/index.html', context)
+
+def authors(request):
+    paginator = Paginator(Author.objects.all(), 3)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
+    return render(request, 'library/authors.html', {'authors': paged_authors})
+
+def author(request, author_id):
+    return render(request, 'library/author.html', {'author': get_object_or_404(Author, id=author_id)})
+
+
+class BookListView(ListView):
+    model = Book
+    paginate_by = 3
+    template_name = 'library/book_list.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        query = self.request.GET.get('query')
+        if query:
+            queryset = queryset.filter(Q(title__icontains=query) | Q(summary__icontains=query))
+        genre_id = self.request.GET.get('genre_id')
+        if genre_id:
+            queryset = queryset.filter(genre__id=genre_id)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['books_count'] = Book.objects.count()
+        context['books_count'] = self.get_queryset().count()
+        genre_id = self.request.GET.get('genre_id')
+        context['genres'] = Genre.objects.all()
+        if genre_id:
+            context['genre'] = get_object_or_404(Genre, id=genre_id)
+        return context
+
+
+class BookDetailVieiw(FormMixin, DetailView):
+    model = Book
+    template_name = 'library/book_detail.html'
+    form_class = BookReviewForm
+
+    def get_success_url(self):
+        return reverse('book', kwargs={'pk': self.object.id})
+
+    def post(self, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            messages.error(self.request, "You're posting to much")
+            return self.form_invalid(form)
+    
+    def form_valid(self, form):
+        form.instance.book = self.get_object()
+        form.instance.reader = self.request.user
+        form.save()
+        messages.success(self.request, 'Your review has been posted!')
+        return super().form_valid(form)
+
+    def get_initial(self):
+        return {
+            'book': self.get_object(),
+            'reader': self.request.user,
+        }
+
+
+class UserBookListView(LoginRequiredMixin, ListView):
+    model = BookInstance
+    template_name = 'library/user_book_list.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(reader=self.request.user).order_by('due_back')
+        return queryset
+
+
+class UserBookInstanceCreateView(LoginRequiredMixin,CreateView):
+    model = BookInstance
+    # fields = ('book', 'due_back')
+    form_class = BookInstanceForm
+    template_name = 'library/user_bookinstance_form.html'
+    success_url = reverse_lazy('user_books')
+
+    def form_valid(self, form):
+        form.instance.reader = self.request.user
+        form.instance.status = 'r'
+        messages.success(self.request, 'Book reserved. ')
+        return super().form_valid(form)
+
+
+class UserBookInstanceUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = BookInstance
+    # fields = ('book', 'due_back')
+    form_class = BookInstanceUpdateForm
+    template_name = 'library/user_bookinstance_form.html'
+    success_url = reverse_lazy('user_books')
+
+    def form_valid(self, form):
+        form.instance.reader = self.request.user
+        form.instance.status = 't'
+        messages.success(self.request, 'Book taken or extended. ')
+        return super().form_valid(form)
+
+    def test_func(self):
+        book_instance = self.get_object()
+        return self.request.user == book_instance.reader
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['book_instance'] = self.get_object()
+        if context['book_instance'].status == 't':
+            context['action'] = 'Extend'
+        else:
+            context['action'] = 'Take'
+        return context
+
+class UserBookInstanceDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = BookInstance
+    template_name = 'library/user_bookinstance_delete.html'
+    success_url = reverse_lazy('user_books')
+
+    def test_func(self):
+        book_instance = self.get_object()
+        return self.request.user == book_instance.reader
+
+    def form_valid(self, form):
+        book_instance = self.get_object()
+        if book_instance.status == 't':
+            messages.success(self.request, 'book returned and burned. ')
+        else:
+            messages.success(self.request, 'book reservation canceled.' )
+        return super().form_valid(form)
+        
